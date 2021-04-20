223857c71bcedc0d788d377903c68ab599aaa1b2
Mainuddinhasansakib <sakibuddin831@gmail.com>
2021-04-20 23:36:50 +0600

    done

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
 // Connect to Mongodb
 const MongoClient = require("mongodb").MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abcuj.mongodb.net//${process.env.DB_NAME}?retryWrites=true&w=majority`;
